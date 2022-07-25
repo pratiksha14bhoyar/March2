8e9ca7ec57ef1a7ef0b9b23f5504f0f8e5b3b5f7
pratiksha Bhoyar <pratiksha Bhoyar@LAPTOP-13L2UAO3>
2022-07-25 23:09:09 +0530

    This is 2nd commit

--- src/com/aspire/java/demo/Sample.java
+++ src/com/aspire/java/demo/Sample.java
@@ -5,6 +5,10 @@ public class Sample
 	public static void main(String[] args)
 	{
 		System.out.println("Hi");
+		int a=10;
+		int b=20;
+		int sum=a+b;
+		System.out.println(sum);
 	}
 
 }
---
+++ src/com/aspire/java/demo/Sample2.java
@@ -0,0 +1,12 @@
+package com.aspire.java.demo;
+
+public class Sample2
+{
+  public static void main(String[] args) 
+  {
+	int p=50;
+	int q=30;
+	int sub=p-q;
+	System.out.println(sub);
+ }
+}
